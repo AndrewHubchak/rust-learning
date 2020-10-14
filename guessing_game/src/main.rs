a42c5e43231f3afe944ea020ef7a0cc61e1b37d6
Andrew Hubchak <andrew.hubchak@gmail.com>
2020-10-14 21:14:02 +0300

    add loop

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -18,64 +18,69 @@ fn main() {
     // but exclusive on the upper bound 
     // so [0, 101)
 
-    let ref mut guess = String::new();
-    // v1: let mut guess = String::new();
-    // v2: let mut guess = &mut String::new();
-    // v3: let ref mut guess = String::new();
-    //
-    // v2 and v3 are eqvivalent
-    // this is a rust feature called `references`
-    //
-    // !Note
-    // !References are one the the rust key features
-    //
-    // with v1 you should call read_line as
-    // io::stdin().read_line(&mut guess)
-    //
-    // let is immutable by default
-    // it is called 'variable binding' instead of just variabled as in other languages
-    // it accepts a `pattern` on the right side
-    //
-    // String is a growable UTF-8 encoded bit of text
-    //
-    // ::new() was called instead of .new()
-    // because this is an `associated function` of a particular type.
-    // That is to say, it's associated with String type itself, rather that a 
-    // particular instance of the String type. In C++ this would be a `static member function`,
-    // aka `static method`
+    loop {
+        let ref mut guess = String::new();
+        // v1: let mut guess = String::new();
+        // v2: let mut guess = &mut String::new();
+        // v3: let ref mut guess = String::new();
+        //
+        // v2 and v3 are eqvivalent
+        // this is a rust feature called `references`
+        //
+        // !Note
+        // !References are one the the rust key features
+        //
+        // with v1 you should call read_line as
+        // io::stdin().read_line(&mut guess)
+        //
+        // let is immutable by default
+        // it is called 'variable binding' instead of just variabled as in other languages
+        // it accepts a `pattern` on the right side
+        //
+        // String is a growable UTF-8 encoded bit of text
+        //
+        // ::new() was called instead of .new()
+        // because this is an `associated function` of a particular type.
+        // That is to say, it's associated with String type itself, rather that a 
+        // particular instance of the String type. In C++ this would be a `static member function`,
+        // aka `static method`
 
-    io::stdin().read_line(guess)
-        .ok()
-        .expect("Read has failed");
-    
-    // io.stdin() or std::io::stdin()
-    // This particular `associated function` returns a handle to the standard input for your
-    // terminal
-    //
-    // .read_line(&mut guess) is used to get input from $USER
-    // read_line is a method and not a `associated function`
-    //
-    //
-    // if I don't use
-    // .ok()
-    // .expect("Read has failed")
-    //
-    // Rust compiler will give a warning.
-    // We should handle all errors properly in rust,
-    // but as this application is not very critical,
-    // we can use .ok().expect() just want to crash on error
-    // and currently for us this is okay
+        io::stdin().read_line(guess)
+            .ok()
+            .expect("Read has failed");
+        
+        // io.stdin() or std::io::stdin()
+        // This particular `associated function` returns a handle to the standard input for your
+        // terminal
+        //
+        // .read_line(&mut guess) is used to get input from $USER
+        // read_line is a method and not a `associated function`
+        //
+        //
+        // if I don't use
+        // .ok()
+        // .expect("Read has failed")
+        //
+        // Rust compiler will give a warning.
+        // We should handle all errors properly in rust,
+        // but as this application is not very critical,
+        // we can use .ok().expect() just want to crash on error
+        // and currently for us this is okay
 
-    // some variable shadowing here :)
-    let guess : u32 = guess.to_string().trim().parse()
-        .ok()
-        .expect("Please type a number and not some crap");
+        // some variable shadowing here :)
+        let guess : u32 = guess.to_string().trim().parse()
+            .ok()
+            .expect("Please type a number and not some crap");
 
-    println!("Your number is: {}", guess);
+        println!("Your number is: {}", guess);
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less      => println!("Your number is to small"),
-        Ordering::Greater   => println!("Your number is to big"),
-        Ordering::Equal     => println!("You win!!! Hooray!!!"),
+        match guess.cmp(&secret_number) {
+            Ordering::Less      => println!("Your number is to small"),
+            Ordering::Greater   => println!("Your number is to big"),
+            Ordering::Equal     => { 
+                println!("You win!!! Hooray!!!");
+                break;
+            }
+        }
     }
 }
