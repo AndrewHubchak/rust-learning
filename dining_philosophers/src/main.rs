536ea8b652508bd3d75eac7478c769108595ced4
Andrew Hubchak <andrew.hubchak@gmail.com>
2020-10-16 17:24:35 +0300

    add basic threads usage

--- dining_philosophers/src/main.rs
+++ dining_philosophers/src/main.rs
@@ -1,3 +1,4 @@
+use std::time;
 use std::thread;
 
 struct Trainee {
@@ -15,7 +16,7 @@ impl Trainee {
     fn eat(&self) {
         println!("{} is eating.", self.name);
         
-        thread::sleep_ms(1000);
+        thread::sleep(time::Duration::new(1, 0));
 
         println!("{} is done eating.", self.name);
     }
@@ -30,8 +31,15 @@ fn main() {
         Trainee::new("Petro Bratash"),
     ];
 
-    for p in &trainees {
-        p.eat();
+    let handles: Vec<_> = trainees.into_iter().map(|p| {
+        thread::spawn(move || {
+            p.eat();
+        })
+    }).collect();
+
+
+    for h in handles {
+        h.join().unwrap();
     }
 }
 
